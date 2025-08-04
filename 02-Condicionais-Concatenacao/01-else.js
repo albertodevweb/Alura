be4466ec4f61a6bf821f3118a1c400ec5430ac75
albertodevweb <albertodevweb@gmail.com>
2025-08-03 21:53:44 -0300

    Trabalhando com condicionais e loops

---
+++ 02-Condicionais-Concatenacao/01-else.js
@@ -0,0 +1,7 @@
+let numeroSecreto = 5;
+let chute =  prompt("Escolha um número entre 1 e 10");
+if(numeroSecreto == chute){
+  console.log(`Isso ai ! Você descobriu o número secreto${numeroSecreto}`);
+}else{
+  console.log('Você errou !');
+}
