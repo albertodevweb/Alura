cbf133fc7fdcd117edb9b7331999f6b297ea02da
albertodevweb <albertodevweb@gmail.com>
2025-08-04 21:56:09 -0300

    Atualização curso

---
+++ 02-Logica-da-Programacao-Funcoes-Listas/app.js
@@ -0,0 +1,5 @@
+let titulo = document.querySelector("h1")
+titulo.innerHTML = "Jogo do número Secreto";
+
+let paragrafo = document.querySelector("p");
+paragrafo.innerHTML = "escolha um numero entre 1 e 10"
